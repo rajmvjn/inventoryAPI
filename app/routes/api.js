ee78e4ed352c8aa2089a2e9f3964b294e713f0ab
Rajesh Vijayan <A-8076@ibsplc.com>
2018-03-14 17:15:27 +0530

    auth reloaded

--- app/routes/api.js
+++ app/routes/api.js
@@ -7,7 +7,7 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
 // connection configurations
-/*const mc = mysql.createConnection({
+const mc = mysql.createConnection({
     host: 'inventorydatabase.cisctc4b8ieq.ap-south-1.rds.amazonaws.com',
     user: 'rajmvjn',
     password: 'myamazon',
@@ -15,9 +15,28 @@ router.use(bodyParser.urlencoded({ extended: false }));
 });
 
 // connect to database
-mc.connect();*/
+mc.connect();
 
 router.post('/api/authenticate', function(req, res) {
+    var userData = req.body;
+
+    mc.query("SELECT id,name FROM user WHERE uname=? AND pwd = ?;", [userData.uname, userData.pwd], function (error, results, fields) {
+        if (error){
+            throw error;
+        } else{
+            if(results.length > 0){
+                return res.send({ error: false, data: results, message: 'User authenticated' });
+            }else{
+                return res.send({ error: true, data: results, message: 'User authentication failed' });
+            }
+        }
+    });
+	
+	return res.send({ error: false, data: {}, message: 'User authenticated' });
+
+});
+
+router.get('/api/authenticate', function(req, res) {
     var userData = req.body;
 
    /* mc.query("SELECT id,name FROM user WHERE uname=? AND pwd = ?;", [userData.uname, userData.pwd], function (error, results, fields) {
@@ -31,7 +50,7 @@ router.post('/api/authenticate', function(req, res) {
             }
         }
     });*/
-	
+	console.log(1);
 	return res.send({ error: false, data: {}, message: 'User authenticated' });
 
 });
